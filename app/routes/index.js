7f32132df67ac4e3b2237280f87a9c6c77e662c6
Ilduchea <ilduchea@gmail.com>
2017-06-02 10:23:34 -0700

    add CRUD methods to routes. Add base structure to site layout.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return Ember.RSVP.hash({
+      questions: this.store.findAll('question')
+    });
+  },
+  actions: {
+    saveQuestion(params) {
+      var newQuestion = this.store.createRecord('question', params);
+      newQuestion.save();
+      this.transitionTo('index');
+    }
+  }
 });
--- app/routes/question.js
+++ app/routes/question.js
@@ -1,4 +1,53 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(params) {
+    return Ember.RSVP.hash({
+      question: this.store.findRecord('question', params.question_id)
+    });
+  },
+  actions: {
+    updateQuestion(question, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          question.set(key,params[key]);
+        }
+      });
+      question.save();
+      this.transitionTo('index');
+    },
+    deleteQuestion(question) {
+      if (confirm('Are you sure you want to delete this question?')) {
+        var answer_deletions = question.get('answers').map(function(answer) {
+          return answer.destroyRecord();
+        });
+        Ember.RSVP.all(answer_deletions).then(function() {
+          return question.destroyRecord();
+        });
+        this.transitionTo('index');
+      }
+    },
+    saveAnswer(params) {
+      var newAnswer = this.store.createRecord('answer', params);
+      var question = params.question;
+      question.get('answers').addObject(newAnswer);
+      newAnswer.save().then(function() {
+        return question.save();
+      });
+      transition.refresh();
+    },
+    updateAnswer(answer, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          answer.set(key,params[key]);
+        }
+      });
+      answer.save();
+      transition.refresh();
+    },
+    destroyAnswer(answer) {
+      answer.destroyRecord();
+      transition.refresh();
+    }
+  }
 });
