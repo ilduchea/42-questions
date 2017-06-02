3223af639a279d34f3cc9c38b3a347c49df963fd
Ilduchea <ilduchea@gmail.com>
2017-06-02 13:28:23 -0700

    add update question.

--- app/components/update-question.js
+++ app/components/update-question.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  showUpdateForm: false,
+  actions: {
+    updateQuestion(question) {
+      var params = {
+        question: this.get('question'),
+        notes: this.get('notes'),
+        author: this.get('author'),
+      };
+      this.set('showUpdateForm', false);
+      this.sendAction('updateQuestion', question, params);
+    },
+    showUpdateForm() {
+      this.set('showUpdateForm', true);
+    }
+  }
 });
