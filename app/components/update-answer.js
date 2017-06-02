002a117278c13e7038cd35b2b062ce359dbfc8e6
Ilduchea <ilduchea@gmail.com>
2017-06-02 14:05:55 -0700

    add update answer

--- app/components/update-answer.js
+++ app/components/update-answer.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  showUpdateForm: false,
+  actions: {
+    updateAnswer(answer) {
+      debugger
+      var params = {
+        author: this.get('author'),
+        answer: this.get('answer1'),
+      };
+      this.set('showUpdateForm', false);
+      this.sendAction('updateAnswer', answer, params);
+    },
+    showUpdateForm() {
+      this.set('showUpdateForm', true);
+    }
+  }
 });
