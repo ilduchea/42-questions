a80f4d13f6ee03fd9382c4eae7ecf0d70082f047
Ilduchea <ilduchea@gmail.com>
2017-06-02 11:33:02 -0700

    add question creation and listing.

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -1,4 +1,24 @@
 import Ember from 'ember';
+import moment from 'moment';
 
 export default Ember.Component.extend({
+  addNewQuestion: false,
+  actions: {
+    questionFormShow() {
+      this.set('addNewQuestion', true);
+    },
+    questionFormHide() {
+      this.set('addNewQuestion', false);
+    },
+    saveQuestion () {
+      var params = {
+        author: this.get('author'),
+        question: this.get('question'),
+        notes: this.get('notes'),
+        created: moment().format('MMMM Do YYYY, h:mm:ss a')
+      };
+      this.set('addNewQuestion', false);
+      this.sendAction('saveQuestion', params);
+    }
+  }
 });
